681167c42cfe711e43444f1bcf5af2a5a6682b18
Michael Leonffu <leonffu.michael@gmail.com>
2021-12-26 05:06:58 -0700

    Updated to match meowlightbot needs

--- config/config_TEMPLATE.py
+++ config/config_TEMPLATE.py
@@ -1,31 +1,15 @@
-DISCORD_TOKEN = "teehee"
-DB_URI = "mongodb+srv://tee:hee@bigcluster.net"
-USER_ID = 0
-STATS_TRACKER_CHANNEL_ID = 1010101029393
-ADMIN_ID = 1010101029393
+# Preferences defaults
+PREFERENCES_PATH = "./config/preferences.json"
+PREFERENCES_DEFAULTS = {
+    'on_time': 1200,
+    'off_time': 300
+}
 
-DEV_MODE = False
-PREFIX = '%'
-PREFIXES = ('a!', 'annali!', 'annali ', 'annali', 'anna li ', 'anna li')
-COGS_LIST = ['cogs.kakTracker', 'cogs.misc', 'cogs.testing', 'cogs.voice', 'cogs.metrics', 'cogs.waifureader', 'cogs.cat', 'cogs.admin']
-
-EMOJI = [
-	":purple_heart:",
-	":brown_heart:",
-	":blue_heart:",
-	":green_heart:",
-	":yellow_heart:",
-	":orange_heart:",
-	":heart:",
-	":white_heart:"
-]
-EMOJI_SEASONAL = [
-	":purple_heart:",
-	":brown_heart:",
-	":blue_heart:",
-	":green_heart:",
-	":yellow_heart:",
-	":orange_heart:",
-	":heart:",
-	":white_heart:"
-]
+# Bot configureation
+DISCORD_TOKEN = "from https://discord.com/developers"
+ADMIN_ID = 420
+USER_ID = 999
+DEV_MODE = True
+PREFIX = '!!'
+PREFIXES = ('johnsmith ', 'john smith ')
+COGS_LIST = ['cogs.misc', 'cogs.testing', 'cogs.admin']
